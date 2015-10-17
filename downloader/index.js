f2cd5edee3532025847a25ad00699a7659cd25e8
Nikhil Kothari <nikhilk@live.com>
2015-10-17 12:58:08 -0700

    CLI implementation

--- downloader/index.js
+++ downloader/index.js
@@ -2,3 +2,46 @@
 // DeviantArt Downloader
 //
 
+var nomnom = require('nomnom');
+
+var optionsSpec = {
+  clientId: {
+    netavar: 'id',
+    full: 'client-id',
+    help: 'DeviantArt client ID',
+    required: true
+  },
+  clientSecret: {
+    metavar: 'secret',
+    full: 'client-secret',
+    help: 'DeviantArt client secret',
+    required: true
+  },
+  source: {
+    metavar: 'collection',
+    help: 'Source collection. All if unspecified.',
+    default: '<all>'
+  },
+  target: {
+    metavar: 'dir',
+    help: 'Target directory',
+    required: true
+  },
+  artist: {
+    flag: true,
+    help: 'Create subdirectories for artist names',
+    default: false
+  }
+};
+
+var options = nomnom.script('downloader')
+                     .options(optionsSpec)
+                     .nocolors()
+                     .parse();
+
+function main() {
+  console.dir(options);
+}
+
+main();
+
